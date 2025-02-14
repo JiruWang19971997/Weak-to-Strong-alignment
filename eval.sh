5c8f50a8861d58a054e93faa4fed9b976bc3b65e
JiruWang19971997 <wmere39@gmail.com>
2025-02-14 13:11:17 +0800

    1

---
+++ eval.sh
@@ -0,0 +1,3 @@
+accelerate launch  eval.py --model_path /root/weak_to_strong_2 --tokenizer_path /root/weak_to_strong_2 --data_path /root/train_2
+
+accelerate launch  eval.py --model_path /root/weak_to_strong_4 --tokenizer_path /root/weak_to_strong_2 --data_path /root/train_2
